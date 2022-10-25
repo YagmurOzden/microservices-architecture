d0abbac9aa1559d622eba90ae9a561f790959a27
yagmurozden <yagmurozden08@hotmail.com>
2022-10-25 15:31:23 +0300

    microservices with go example part 1 v0.0.0

---
+++ microservices architecture with GOlang/README.md
@@ -0,0 +1,1 @@
+Building and learning microservicews with GOlang
---
+++ microservices architecture with GOlang/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/yagmurozden/microservices/handlers"
+)
+
+func main() {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	http.ListenAndServe(":9090", sm)
+}
