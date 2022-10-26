9fced1cae847f9113ffb237864e4103ccbe50488
yagmurozden <yagmurozden08@hotmail.com>
2022-10-26 15:11:04 +0300

    http Server added  v0.0.2

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/yagmurozden/microservices/handlers"
 )
 
 func main() {
-	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	l := log.New(os.Stdout, "test API - ", log.LstdFlags)
 
 	hh := handlers.NewHello(l)
 	gh := handlers.NewGoodbye(l)
@@ -18,5 +19,24 @@ func main() {
 	sm.Handle("/goodbye", gh)
 	sm.Handle("/", hh)
 
-	http.ListenAndServe(":9090", sm)
+	s := http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+	s.ListenAndServe()
+	// go func() {
+	// 	err := s.ListenAndServe()
+	// 	if err != nil {
+	// 		l.Fatal(err)
+	// 	}
+	// }()
+	//it will wait the process and it will shot down the service, if we want to upgrade or something we may use it.
+	//if the handkers still working of this time it will force shotdown
+
+	// tc, cancel := context.WithTimeout(context.Background(), 300*time.Second)
+	// fmt.Println("cancel: ", cancel)
+	// s.Shutdown(tc)
 }
